3e095c5b2e7c8f9063e5efb37bd42c11ad53e86d
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-04-06 00:06:02 +0200

    Consistent use of tabs

--- quamotion/__init__.py
+++ quamotion/__init__.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 
 def add_quamotion_extensions(driver):
-        driver.command_executor._commands["homescreen"] = ('POST', '/session/$sessionId/wda/homescreen')
-        driver.command_executor._commands["get_installed_apps"] = ('GET', '/quamotion/device/$deviceId/app?strict&all')
-        driver.command_executor._commands["get_running_processes"] = ('GET', '/quamotion/device/$deviceId/process?strict')
-        driver.command_executor._commands["launch_app"] = ('POST', '/quamotion/device/$deviceId/app/$appId/launch?strict')
-        driver.command_executor._commands["uninstall_app"] = ('DELETE', '/quamotion/device/$deviceId/app/$appId?strict')
-        driver.command_executor._commands["kill_app"] = ('POST', '/quamotion/device/$deviceId/app/$appId/kill?strict')
-        
+	driver.command_executor._commands["homescreen"] = ('POST', '/session/$sessionId/wda/homescreen')
+	driver.command_executor._commands["get_installed_apps"] = ('GET', '/quamotion/device/$deviceId/app?strict&all')
+	driver.command_executor._commands["get_running_processes"] = ('GET', '/quamotion/device/$deviceId/process?strict')
+	driver.command_executor._commands["launch_app"] = ('POST', '/quamotion/device/$deviceId/app/$appId/launch?strict')
+	driver.command_executor._commands["uninstall_app"] = ('DELETE', '/quamotion/device/$deviceId/app/$appId?strict')
+	driver.command_executor._commands["kill_app"] = ('POST', '/quamotion/device/$deviceId/app/$appId/kill?strict')
+	
 def device(deviceId):
 	driver = webdriver.Remote(
 		command_executor='http://localhost:17894/wd/hub',
@@ -17,11 +17,11 @@ def device(deviceId):
 			'applicationType': 'Device',
 			'deviceId': deviceId
 		})
-        
-        driver.deviceId = deviceId
+	
+	driver.deviceId = deviceId
 
-        add_quamotion_extensions(driver)
-        return driver
+	add_quamotion_extensions(driver)
+	return driver
 
 def web(deviceId):
 	driver = webdriver.Remote(
@@ -32,29 +32,29 @@ def web(deviceId):
 			'applicationType': 'Web',
 			'deviceId': deviceId
 		})
-        
-        driver.deviceId = deviceId
+	
+	driver.deviceId = deviceId
 
-        add_quamotion_extensions(driver)
-        return driver
-        
+	add_quamotion_extensions(driver)
+	return driver
+	
 def home_screen(self):
-        return self.execute('homescreen', {})
+	return self.execute('homescreen', {})
 
 def get_installed_apps(self):
-        return self.execute('get_installed_apps', { 'deviceId': self.deviceId } )
+	return self.execute('get_installed_apps', { 'deviceId': self.deviceId } )
 
 def get_running_processes(self):
-        return self.execute('get_running_processes', { 'deviceId': self.deviceId } )
+	return self.execute('get_running_processes', { 'deviceId': self.deviceId } )
 
 def launch_app(self, app_id):
-        return self.execute('launch_app', { 'deviceId': self.deviceId, 'appId': app_id } )
+	return self.execute('launch_app', { 'deviceId': self.deviceId, 'appId': app_id } )
 
 def uninstall_app(self, app_id):
-        return self.execute('uninstall_app', { 'deviceId': self.deviceId, 'appId': app_id } )
+	return self.execute('uninstall_app', { 'deviceId': self.deviceId, 'appId': app_id } )
 
 def kill_app(self, app_id):
-        return self.execute('kill_app', { 'deviceId': self.deviceId, 'appId': app_id } )
+	return self.execute('kill_app', { 'deviceId': self.deviceId, 'appId': app_id } )
 
 webdriver.Remote.home_screen = home_screen
 webdriver.Remote.get_installed_apps = get_installed_apps
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='quamotion',
-      version='0.62',
+      version='0.62.1',
       description='Python client for the Quamotion WebDriver',
       url='http://github.com/quamotion/python',
       author='Quamotion',
