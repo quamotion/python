70ad50a71d6b4de512e2106045ef24ecb5cce0a7
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-05-15 13:01:23 +0200

    Bump version number for release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='quamotion',
-      version='0.64.1',
+      version='0.64.2',
       description='Python client for the Quamotion WebDriver',
       url='http://github.com/quamotion/python',
       author='Quamotion',
